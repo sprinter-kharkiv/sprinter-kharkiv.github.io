6a077560a4f3f3380db9c20f61d599119c393757
Alex_Z <zagatskiy@yandex.ru>
2016-11-25 10:03:23 +0200

    descr

---
+++ projects/adoption_1/js/main.js
@@ -0,0 +1,35 @@
+﻿
+$(document).ready(function() {
+
+    // Mobile menu
+    $('.j-menu-trig').on('click', function () {
+        $('.start-screen_header').toggleClass('vis-menu')
+    });
+    $('.header_nav').on('click', function () {
+        $('.start-screen_header').removeClass('vis-menu')
+    });
+
+
+    // Header Scroll
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 100) {
+            $('.header').addClass('fixed');
+        } else {
+            $('.header').removeClass('fixed');
+        }
+    });
+
+    // parallax for background
+    $(".scroll-bg").each(function(){
+        var $bgobj = $(this);
+
+        $(window).scroll(function(){
+            var yPos = -($(window).scrollTop() /  $bgobj.data("speed"));
+            var coords = "50%" + yPos + "px";
+
+            $bgobj.css(
+                "background-position" , coords);
+        })
+    });
+});
---
+++ projects/adoption_1/readme.txt
@@ -0,0 +1,19 @@
+
+обновить локальный репозиторий
+$ git pull origin master
+
+проверить состояние-
+$ git status
+
+добавить все новые изменения, файлы, папки
+$git add .
+
+закоммитить
+$git commit -m "describe"
+
+Запись изменений на гитхаб-
+
+$ git push -u origin master
+
+sprinter-kharkiv
+a0955673318
