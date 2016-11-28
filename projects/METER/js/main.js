0082589d31eb6d027532d806d8fe66f3f0367186
Alex_Z <zagatskiy@yandex.ru>
2016-11-28 17:50:25 +0200

    desc

---
+++ projects/METER/js/main.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+
+    $('.j-go-to').click(function () {
+        var this_el = $(this).parents('.section').index(),
+            scroll_el = '#section-' + (this_el+2);
+        console.log(scroll_el);
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+            $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 500); // анимируем скроолинг к элементу scroll_el
+        }
+    });
+
+
+    $('.j-catalog').click(function (event) { // лoвим клик пo ссылки с id="go"
+        event.preventDefault(); // выключaем стaндaртную рoль элементa
+        $('#overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
+            function () { // пoсле выпoлнения предъидущей aнимaции
+                $('#modal_catalog')
+                    .css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
+                    .animate({opacity: 1, top: '50%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+            });
+    });
+    $('.j-contact').click(function (event) { // лoвим клик пo ссылки с id="go"
+        event.preventDefault(); // выключaем стaндaртную рoль элементa
+        $('#overlay').fadeIn(200, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
+            function () { // пoсле выпoлнения предъидущей aнимaции
+                $('#modal_contact')
+                    .css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
+                    .animate({opacity: 1, top: '50%'}, 100); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+            });
+    });
+    /* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
+    $('#modal_close, #overlay').click(function () { // лoвим клик пo крестику или пoдлoжке
+        $('#modal_contact, #modal_catalog')
+            .animate({opacity: 0, top: '45%'}, 100,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+            function () { // пoсле aнимaции
+                $(this).css('display', 'none'); // делaем ему display: none;
+                $('#overlay').fadeOut(200); // скрывaем пoдлoжку
+            }
+        );
+    });
+});
