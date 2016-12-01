749c0ec4ec91c19de812e11e941d93368f22d2b6
sprinter.kharkiv <sprinter.kharkiv@gmail.com>
2016-12-02 00:13:00 +0200

    desc

--- projects/METER/send.php
+++ projects/METER/send.php
@@ -1,44 +1,59 @@
-<?
+<?php
+function mail_attachment($filename, $path, $mailto, $from_mail, $from_name, $replyto, $subject, $message) {
+    $file = $path.$filename;
+    $file_size = filesize($file);
 
-// Settings
-$to       = $_POST['email'];
-$from        = "OCEANO";
-$subject     = "NEW Oceano catalogo";
-$mainMessage = "Hi, here's the file.";
-$fileatt     = "files/Oceano_catalogo2016_low_new.pdf"; //file location
-$fileatttype = "application/pdf";
-$fileattname = "catalog.pdf"; //name that you want to use to send or you can use the same name
-$headers = "From: $from";
+    $handle = fopen($file, "r,b");
+    $content = fread($handle, $file_size);
+    fclose($handle);
 
-// File
-$file = fopen($fileatt, 'rb');
-$data = fread($file, filesize($fileatt));
-fclose($file);
 
-// This attaches the file
-$semi_rand     = md5(time());
-$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
-$headers      .= "\nMIME-Version: 1.0\n" .
-    "Content-Type: multipart/mixed;\n" .
-    " boundary=\"{$mime_boundary}\"";
-$message = "This is a multi-part message in MIME format.\n\n" .
-    "-{$mime_boundary}\n" .
-    "Content-Type: text/plain; charset=\"iso-8859-1\n" .
-    "Content-Transfer-Encoding: 7bit\n\n" .
-    $mainMessage  . "\n\n";
+    $content = chunk_split(base64_encode($content));
+    $uid = md5(uniqid(time()));
+    $name = basename($file);
 
-$data = chunk_split(base64_encode($data));
-$message .= "--{$mime_boundary}\n" .
-    "Content-Type: {$fileatttype};\n" .
-    " name=\"{$fileattname}\"\n" .
-    "Content-Disposition: attachment;\n" .
-    " filename=\"{$fileattname}\"\n" .
-    "Content-Transfer-Encoding: base64\n\n" .
-    $data . "\n\n" .
-    "-{$mime_boundary}-\n";
 
-// Send the email
-mail($to, $subject, $message, $headers)
+    $header = "From: ".$from_name." <".$from_mail.">\r\n";
+    $header .= "Reply-To: ".$replyto."\r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
+    $header .= "This is a multi-part message in MIME format.\r\n";
+    $header .= "--".$uid."\r\n";
+    $header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+    $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+    $header .= $message."\r\n\r\n";
+    $header .= "--".$uid."\r\n";
+    $header .= "Content-Type: application/pdf; name=\"".$filename."\"\r\n"; // use different content types here
+    $header .= "Content-Transfer-Encoding: base64\r\n";
+    $header .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
+    $header .= $content."\r\n\r\n";
+    $header .= "--".$uid."--";
+    if (mail($mailto, $subject, "", $header)) {
+        echo "mail send ... OK"; // or use booleans here
+    } else {
+        echo "mail send ... ERROR!";
+    }
+}
+$client = $_POST['email'];
+$admin = "info@laudarte.com";    //email for replay
+$reportSubject     = "downloading the catalog";
+$reportMessage = "\n\n"              // replay text 
+    . " $client download our catalog";
+$my_file = "Oceano_catalogo2016_low_new.pdf";
+$my_path = $_SERVER['/files/']."/your_path_here/";
+$my_name = "Oceano";
+$my_mail = "info@laudarte.com";
+$my_replyto = "my_reply_to@mail.net";
+$my_subject = "NEW  catalog Oceano";
+$my_message = "\n\n"
+    . "Thank you for your request,\n
+ATTACHED HERE\n
+Oceano Oltreluce 2016/2017 Catalogue: a technical and essential collection, totally handmade.\n
+Extremely modern furnishing products for privates and supplies.";
 
+mail_attachment($my_file, $my_path, $client, $my_mail, $my_name, $my_replyto, $my_subject, $my_message);
+
+mail($admin, $reportSubject, $reportMessage)
 
 ?>
+
