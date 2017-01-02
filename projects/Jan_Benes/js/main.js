6a72ab5bccbbe137601fa9cc15477d76a6dfda4a
sprinter.kharkiv <sprinter.kharkiv@gmail.com>
2017-01-03 00:24:25 +0200

    desc

--- projects/Jan_Benes/js/main.js
+++ projects/Jan_Benes/js/main.js
@@ -1,14 +1,5 @@
 ﻿$(window).load(function () {
 
-  /*  (function () {
-        var v = document.getElementsByTagName('video')[0];
-        var t = document.getElementById('time');
-        v.addEventListener('timeupdate', function (event) {
-            t.innerHTML = parseInt(v.currentTime) + ' - ' + v.currentTime;
-        }, false);
-    })();
-*/
-
     var video_1 = document.getElementById('whitebox'),
         brake_1 = 2.2,
         brake_2 = 4.4,
@@ -16,34 +7,52 @@
         stop = 5.7,
         brake = brake_1;
 
-
-
-
     video_1.addEventListener("timeupdate", function () {
 
         if (this.currentTime >= brake) {
             this.pause();
         }
         else if (this.currentTime >= stop) {
-            $(this).animate({opacity:'0'}, 500);
+            $(this).animate({opacity: '0'}, 500);
             $('.trig_2').css('display', 'none');
-            $('.present_link').css('display', 'table').animate({opacity:'1'}, 500);
+            $('.present_link').css('display', 'table').animate({opacity: '1'}, 500);
         }
     });
-    $('.present-video').click(function () {
-        video_1.play();
-        $('.trig_1').css('display', 'block');
-    });
-    $('.trig_1').click(function () {
-        video_1.play();
-        brake = brake_2;
-        $(this).css('display', 'none');
-        $('.trig_2').css('display', 'block');
-    });
-    $('.trig_2').click(function () {
-        video_1.play();
-        brake = brake_3;
+    /*
+     $(window).on('scroll', function () {
+     var scroll = $(window).scrollTop();
+     if (scroll >= 100) {
+     video_1.play();
+     $('.trig_1').css('display', 'block');
+
+     }
+     });
+     */
+    $('.present-video').swipe({
+        //Generic swipe handler for all directions
+        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+            video_1.play();
+            $('.trig_1').css('display', 'block');
+        }
     });
 
+
+    $('.trig_1').swipe({
+        //Generic swipe handler for all directions
+        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+            video_1.play();
+            brake = brake_2;
+            $(this).css('display', 'none');
+            $('.trig_2').css('display', 'block');
+        }
+    });
+    $('.trig_2').swipe({
+        //Generic swipe handler for all directions
+        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+            video_1.play();
+            brake = brake_3;
+        }
+    });
 
 });
+
