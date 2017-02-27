e91d9c51e7f7f40726c405fa0b88a4f8d885093d
Alex Zagatskiy <sprinter.kharkiv@gmail.com>
2017-02-27 16:58:17 +0200

    desc

--- projects/lakeside/js/common.js
+++ projects/lakeside/js/common.js
@@ -19,6 +19,43 @@ $(document).ready(function () {
         pager: false
     });
 
+
+    //Floatet label in form action
+    $('.form-control').focusout(function() {
+        $('.form-group').removeClass('focus');
+    });
+    $('.form-control').focus(function() {
+        $(this).closest('.form-group').addClass('focus');
+    });
+    $('select.form-control').change(function() {
+        $(this).closest('.form-group').addClass('filled');
+    });
+
+
+    /// Input Kepress Filled  Focus
+    $('.form-control').keyup(function() {
+        if($(this).val().length > 0){
+            $(this).closest('.form-group').addClass('filled');
+        }
+
+        else{
+            $(this).closest('.form-group').removeClass('filled');
+        }
+    });
+
+    /// Input Check Filled Focus
+    var $formControl = $('.form-control');
+    var values = {};
+    var validate =    $formControl.each(function() {
+        if($(this).val().length > 0){
+            $(this).closest('.form-group').addClass('filled');
+        }
+        else{
+            $(this).closest('.form-group').removeClass('filled');
+        }
+    });
+
+
     // Header Scroll
     $(window).on('scroll', function () {
         var scroll = $(window).scrollTop();
