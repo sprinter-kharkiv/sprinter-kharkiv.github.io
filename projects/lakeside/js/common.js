a4a1430af393554ea227141b751cb4f8e2fffc65
Alex Zagatskiy <sprinter.kharkiv@gmail.com>
2017-02-28 13:20:51 +0200

    desc

--- projects/lakeside/js/common.js
+++ projects/lakeside/js/common.js
@@ -3,6 +3,14 @@
  */
 $(document).ready(function () {
 
+    // Page Scroll
+    $('.j-go-to').click(function () {
+        var scroll_el = $(this).attr("href");
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 500);
+        }
+    });
+
     // Mobile menu
     $('.j-menu-trig').on('click', function () {
         $('.header').toggleClass('vis-menu')
