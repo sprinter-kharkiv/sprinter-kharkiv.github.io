be4296b1962220813d94769739cc8bd4530d4932
sprinter.kharkiv <sprinter.kharkiv@gmail.com>
2017-03-16 22:51:48 +0200

    d

--- projects/marketlend/src/javascript/common.js
+++ projects/marketlend/src/javascript/common.js
@@ -25,32 +25,12 @@ $(document).ready(function() {
         }
     });
 
- $('.range').rangeslider(
- {
 
-    // Feature detection the default is `true`.
-    // Set this to `false` if you want to use
-    // the polyfill also in Browsers which support
-    // the native <input type="range"> element.
-    polyfill: true,
+    $(".range_summ").asRange({
+        keyboard: true,
+        tip: true
 
-    // Default CSS classes
-    rangeClass: 'rangeslider',
-    disabledClass: 'rangeslider--disabled',
-    horizontalClass: 'rangeslider--horizontal',
-    verticalClass: 'rangeslider--vertical',
-    fillClass: 'rangeslider__fill',
-    handleClass: 'rangeslider__handle',
-
-    // Callback function
-    onInit: function() {},
-
-    // Callback function
-    onSlide: function(position, value) {},
-
-    // Callback function
-    onSlideEnd: function(position, value) {}
-});
+    });
 
 
 
