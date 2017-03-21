83e7ef23cea4ad14c6e849c1b6059bcaa18fa8f7
sprinter.kharkiv <sprinter.kharkiv@gmail.com>
2017-03-22 00:28:25 +0200

    d

--- projects/marketlend/src/javascript/common.js
+++ projects/marketlend/src/javascript/common.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
     $(window).on('scroll', function () {
@@ -14,7 +14,7 @@ $(document).ready(function() {
             $('.header').removeClass('fixed');
         }
         // counter
-        if(scrTop>elem.offset().top - $(window).height() + 50 ) {
+        if (scrTop > elem.offset().top - $(window).height() + 50) {
             elem.animateNumber(
                 {
                     number: value,
@@ -33,15 +33,67 @@ $(document).ready(function() {
     // when the value is changed
 
     $(".range_summ").on('asRange::change', function (e) {
-        var summ = $('#summ').val();
-        var time = $('#time').val();
-        var monthly_repaymant = $('.monthly_repaymant');
-        var total_payable = $('.total_payable');
-        monthly_repaymant.html(summ);
-        total_payable.html(time);
+        var limit = $('#summ').val(),
+            term = $('#time').val(),
+            monthly_repaymant = $('.monthly_repaymant'),
+            total_payable = $('.total_payable'),
+            invoice_value = 50000,
+            risk_band_interest_rate = 1.3 / 100,
+            facility_fee = 0.42 / 100,
+
+            monthly_repaymant_sum = (invoice_value*risk_band_interest_rate)+(limit-invoice_value)*facility_fee,
+            total_payable_sum = ((invoice_value*risk_band_interest_rate) +((limit-invoice_value)*facility_fee))*term;
+
+        monthly_repaymant.html(monthly_repaymant_sum.toFixed(2));
+        total_payable.html(total_payable_sum.toFixed(2));
+    });
+    //mzonthly_repaymant = $('.monthly_repaymant'),
+    // Функция проверки полей формы
+    var email = $('#email');
+    var phone = $('#phone');
+    var input = $('.text_input');
+
+    phone.focusout(function () {
+        var re = /^\d[\d\(\)\ -]{4,14}\d$/;
+        var myPhone = phone.val();
+        var valid = re.test(myPhone);
+        if (valid) {
+            $(this).addClass('checked');
+            $(this).removeClass('has_error');
+        } else {
+            $(this).removeClass('checked');
+            $(this).addClass('has_error');
+        }
+        if (myPhone == '') {
+            $(this).removeClass('checked');
+            $(this).removeClass('has_error');
+        }
     });
 
+    email.focusout(function () {
+        console.log('lost');
+        var re = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+        var myMail = email.val();
+        var valid = re.test(myMail);
+        if (valid) {
+            $(this).addClass('checked');
+            $(this).removeClass('has_error');
+        } else {
+            $(this).removeClass('checked');
+            $(this).addClass('has_error');
+        }
+        if (myMail == '') {
+            $(this).removeClass('checked');
+            $(this).removeClass('has_error');
+        }
+    });
+    input.focusout(function () {
+        if (email.hasClass('checked') && phone.hasClass('checked')) {
+            $('.calculator_text').removeClass('hidden');
+            $('.form_btn').removeClass('disabled');
+        }
 
+    })
 
 
 });
