6925eebd6dc7e77a3f57fb39827c95dd44e88997
Alex Zagatskiy <sprinter.kharkiv@gmail.com>
2017-12-07 15:06:48 +0200

    w

--- js/main.js
+++ js/main.js
@@ -1,51 +1,11 @@
 
 $(document).ready(function () {
 
-    /* Collapsed section action */
-    $('.collapse-btn').on('click', function (event) {
-        event.preventDefault();
-        $(this).toggleClass('active');
-        $('.collapsed-holder').slideToggle(500);
+    $('.menu-trigger').on('click', function () {
+        $('.header').toggleClass('vis-menu')
     });
-
-$('.menu-trigger').on('click', function () {
-    $('.header').toggleClass('vis-menu')
-});
-
-    //Floatet label in form action
-    $('.form-control').focusout(function () {
-        $('.form-group').removeClass('focus');
+    $('.navigation_link').on('click', function(){
+        $('.header').removeClass('vis-menu')
     });
-    $('.form-control').focus(function () {
-        $(this).closest('.form-group').addClass('focus');
-    });
-    $('select.form-control').change(function () {
-        $(this).closest('.form-group').addClass('filled');
-    });
-
-
-    /// Input Kepress Filled  Focus
-    $('.form-control').keyup(function () {
-        if ($(this).val().length > 0) {
-            $(this).closest('.form-group').addClass('filled');
-        }
-
-        else {
-            $(this).closest('.form-group').removeClass('filled');
-        }
-    });
-
-    /// Input Check Filled Focus
-    var $formControl = $('.form-control');
-    var values = {};
-    var validate = $formControl.each(function () {
-        if ($(this).val().length > 0) {
-            $(this).closest('.form-group').addClass('filled');
-        }
-        else {
-            $(this).closest('.form-group').removeClass('filled');
-        }
-    });
-
 
 });
