21b6c4e6ece9607d7c08b68b2c2b12a71ad4ac80
Alex <sprinter.kharkiv@gmail.com>
2018-03-25 23:16:28 +0300

    h

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 $(document).ready(function () {
 
     $('.menu-trigger').on('click', function () {
-        $('.header').toggleClass('vis-menu')
+        $('.header').toggleClass('vis-menu');
+        $('.wrapper').toggleClass('vis-menu')
     });
     $('.navigation_link').on('click', function(){
         $('.header').removeClass('vis-menu')
