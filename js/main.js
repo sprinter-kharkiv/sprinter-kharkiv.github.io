10dbbf1147ee377f363481e2df4b24678871fe00
Alex Zagatskiy <sprinter.kharkiv@gmail.com>
2018-03-26 10:57:06 +0300

    d

--- js/main.js
+++ js/main.js
@@ -6,7 +6,33 @@ $(document).ready(function () {
         $('.wrapper').toggleClass('vis-menu')
     });
     $('.navigation_link').on('click', function(){
-        $('.header').removeClass('vis-menu')
+        $('.header').removeClass('vis-menu');
+        $('.wrapper').removeClass('vis-menu')
     });
 
+
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function(){ 
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 600);
+    return false; });
+
+    if($page.scrollTop() == 0){
+		$('.scroll-top').removeClass('visible');
+    }
+
 });
+
+$(window).bind('mousewheel', function(event) {
+	var st = $(this).scrollTop();
+if (event.originalEvent.wheelDelta >= 0 && st >= 600) {
+	$('.scroll-top').addClass('visible');
+}
+else {
+    $('.scroll-top').removeClass('visible');
+};
+});
+$('.scroll-top').on('click', function(){
+	$('.scroll-top').removeClass('visible');
+})
